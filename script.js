4d89f68cae810a9facf16cfc0f85c59ec2fd7253
Lili <111713900+Dzikrillah8@users.noreply.github.com>
2025-10-21 17:58:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// toggle class active search-form
+
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+};
